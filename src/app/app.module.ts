f16a08ed2d65dbc434f034292c6fc6752b07751b
Davidyero <davidyero366@gmail.com>
2020-07-01 01:29:23 -0500

    3. Create base project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app/app.component';
+import { CardComponent } from './shared/components/card/card.component';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
+import { PanelComponent } from './shared/components/panel/panel.component';
+import { BannerComponent } from './shared/components/banner/banner.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HeaderComponent,
+    FooterComponent,
+    PanelComponent,
+    BannerComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
